bffbe18921e2fdb9d08d7c2da6e02db8b6124c11
alighadiri <ghadirini1373@gmail.com>
2019-10-29 00:42:56 +0330

    velocity needs more tune

--- src/Slider.js
+++ src/Slider.js
@@ -5,6 +5,10 @@ import {PanGestureHandler, State} from 'react-native-gesture-handler';
 const indicatorWidth = 15;
 const barHeight = 2;
 const balloonWidth = 50;
+const Directions = {
+  RIGHT:'right',
+  LEFT:'left'
+}
 
 class Slider extends Component {
 
@@ -16,50 +20,116 @@ class Slider extends Component {
     barStartX: undefined,
     velocityX: 0,
     balloonOffset: new Animated.Value(0),
+    direction:undefined
   };
 
   rotateDeg = new Animated.Value(45)
   balloonOffset = new Animated.Value(0)
 
   swipeHandler = (event) => {
-    const xChange = event.nativeEvent.translationX;
-    const velocityX = event.nativeEvent.velocityX;
-    const {previousValue, barWidth} = this.state;
-    // console.log('xChange',xChange)
-    // console.log('velocity',velocityX)
+    const {previousValue, barWidth,currentValue,direction,moving} = this.state;
 
-    const newValue = previousValue + xChange;
+    const deltaX = event.nativeEvent.translationX;
+    const velocityX = event.nativeEvent.velocityX;
+    const newValue = previousValue + deltaX;
+    const newDirection = newValue > currentValue ? Directions.RIGHT : Directions.LEFT
+
+
+    if( deltaX === 0 )
+      return
+
     this.setState({
       currentValue: newValue < 0 ? 0 : newValue > barWidth ? barWidth : newValue,
       velocityX,
+      direction: newDirection
     });
 
+    // console.log('deltaX',deltaX)
+    // console.log('currentValue',currentValue)
+    // console.log('newValue',newValue)
+    // console.log('direction',direction)
+    // console.log('newDirection',newDirection)
+    console.log('velocity',velocityX)
+
+    if(direction !== newDirection || !moving )
+    {
+      this.animate(newDirection )
+      this.setState({
+        moving:true
+      })
+    }
+  };
+
+  resetPosition = () =>{
     Animated.parallel([
       Animated.timing(
         this.rotateDeg,
         {
-          duration:1000,
-          toValue:xChange > 0 ? 25 : 65,
+          duration:100,
+          toValue:45,
           useNativeDriver:true
         }),
       Animated.timing(
         this.balloonOffset,
         {
-          duration:1000,
-          toValue:xChange > 0 ? 25 : -25,
+          duration:100,
+          toValue:0,
           useNativeDriver:true
         })
     ]).start()
-  };
+  }
+
+  static calculateMultiplierByVelocity=(velocityX)=>{
+    if(velocityX === 0)
+      return 1
+
+    let multiplier = 1
+    // const multiplier = Math.abs(Math.ceil( 1000 / velocityX )) / 2
+
+      velocityX < 200 ? multiplier = 1 :
+        velocityX < 600 ? multiplier = 0.4 :
+          velocityX < 900 ? multiplier = 0.2 :
+            multiplier = 0.1
+
+
+    console.log('multiplier',multiplier)
+    console.log('velocityX',velocityX)
+    return multiplier
+  }
+  animate = (newDirection)=>{
+    // console.log('newDDDDDDDDDDDD',newDirection)
+    const { velocityX } = this.state
+    const multiplier = Slider.calculateMultiplierByVelocity(Math.abs(velocityX))
+
+    Animated.parallel([
+      Animated.timing(
+        this.rotateDeg,
+        {
+          duration:250,
+          toValue:newDirection === Directions.RIGHT ? (45 - 30 / multiplier) : (45 + 30 / multiplier),
+          useNativeDriver:true
+        }),
+      Animated.timing(
+        this.balloonOffset,
+        {
+          duration:250,
+          toValue:newDirection === Directions.RIGHT ? 35 / multiplier  : -35 / multiplier ,
+          useNativeDriver:true
+        })
+    ]).start()
+  }
 
   handleStateChange = ({nativeEvent}) => {
     if (nativeEvent.state === State.END) {
-      this.setState({previousValue: this.state.currentValue});
+      this.setState({
+        previousValue: this.state.currentValue,
+        moving:false
+      });
+      this.resetPosition()
     }
   };
 
   measureLayout = (event) => {
-    console.log('eve', event.nativeEvent);
     this.setState({
       barWidth: event.nativeEvent.layout.width,
       // barStartX:event.nativeEvent.layout.width,
@@ -82,9 +152,6 @@ class Slider extends Component {
     // const ballon = this.balloonOffset.setValue(0)
     // const balloonOff =
     // console.log('DEG', rotateDeg);
-    console.log('currentValue - this.balloonOffset', currentValue - this.balloonOffset);
-    console.log('this.balloonOffset', this.balloonOffset);
-    console.log('currentValue', currentValue );
     return (
       <View style={styles.wrapper}>
         <View style={{width: '100%', alignItems: 'center'}}>
@@ -129,16 +196,16 @@ const styles = StyleSheet.create({
     borderTopStartRadius: balloonWidth / 2,
     borderTopEndRadius: balloonWidth / 2,
     borderBottomStartRadius: balloonWidth / 2,
-    transform:[{translateX},{rotate}]
+    transform:[{translateX},{rotate},]
   }),
-  indicator: (translateX) => ({
+  indicator: (left) => ({
     width: indicatorWidth,
     top: -indicatorWidth / 2 - barHeight,
     aspectRatio: 1,
     borderRadius: indicatorWidth / 2,
     marginStart: -indicatorWidth / 2,
     backgroundColor: '#4A26EC',
-    transform: [{translateX}],
+    left,
   }),
   barFill: {
     height: barHeight,
